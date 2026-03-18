9198a3dfdc362953e3ed992c44e3fe228db1193a
sriraj_nsns <nsnsdevilsboy@gmail.com>
2026-03-18 10:54:00 +0530

    UC2: Accept one name via command-line input and greet that user.

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,7 +1,6 @@
 public class HelloApp {
-
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Get the name from command-line argument
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
-
-}
