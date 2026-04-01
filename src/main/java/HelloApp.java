b7a6c464485a0a5e84f07ab62a79366ef238a629
NSNS <nsnsdevilsboy@gmail.com.com>
2026-04-02 01:43:18 +0530

    Save local changes before merge

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,2 +1,19 @@
-public class HelloApp {
+
+public class HelloApp{
+    public static void main(String[] args) {
+
+        String greeting;
+
+        // Check if no arguments are provided
+        if (args.length == 0) {
+            greeting = "Hello, World!";
+        } else {
+            // Join all arguments with ", "
+            String names = String.join(", ", args);
+            greeting = "Hello, " + names + "!";
+        }
+
+        // Print the greeting
+        System.out.println(greeting);
+    }
 }
